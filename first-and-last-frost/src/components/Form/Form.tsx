22194d73362413776d5b3e3c4cce15fe1d600f46
Rebekah Anderson <rebekahcodesgmail.comgit config --global user.email rebekahcodesgmail.com>
2024-08-22 15:09:46 +0100

    added geolocation api call

--- first-and-last-frost/src/components/Form/Form.tsx
+++ first-and-last-frost/src/components/Form/Form.tsx
@@ -9,11 +9,23 @@ function Form() {
     const [userInput, setUserInput] = useState('');
     const [userPostcode, setUserPostcode] = useState('');
 
+    async function getLocation (userPostcode:any){
+        const response = await fetch (`https://api.opencagedata.com/geocode/v1/json?q=${userPostcode}&key=b24754d869804a358fb2a74f3a52a961&language=en&pretty=1`);
+        const data = await response.json();
+        console.log(data)
+        console.log(data.results[0].geometry.lat)
+        console.log(data.results[0].geometry.lng)
+        console.log(userPostcode)
+        }
+   
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         setUserPostcode(userInput)
-        console.log(`submitted postcode is ${userPostcode}`);
-        <Results userPostcode={userPostcode} />; // Call Results with the prop
+        getLocation(userInput)
+        console.log(`submitted postcode is ${userInput}`);
+        
+    
+        <Results userPostcode={userPostcode} />; 
     };    
         
 
@@ -28,7 +40,9 @@ function Form() {
             required
             />
             <button type='submit'>Submit</button>
+            
         </form>
+        
         <Results userPostcode={userPostcode} />
         </>
     )
--- first-and-last-frost/src/components/Results/Results.tsx
+++ first-and-last-frost/src/components/Results/Results.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+//Take in the users postcode from for sumbission (needs to be validated)
+//Do an api geo call with the postcode to get the lat and long codes
+//extract lat and long codes to create a weather api call 
+//Send weather api call with lat and long and return results
+
+
 function Results({userPostcode}:any){
+
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setLongitude] = useState('')
+
+    
+
     return (
         <div className='Results'>
+            <div className="postcode">
           <p>postcode is {userPostcode}</p>
+          </div>
+          
+          <div className="location"></div>
+          <p>location is </p>
         </div>
     );
     }
